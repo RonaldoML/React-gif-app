7f9191d7c759f47fcd1eadb9fa9962a1b203f27d
RonaldoML <rmonserr@espol.edu.ec>
2020-07-12 14:47:12 -0500

    docs agregado

---
+++ docs/precache-manifest.c042b2283e0d7a097ce480492e36edaf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52ee313e0ad4fefba771541ddc030821",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf37ad34fb09310e758e",
+    "url": "/static/css/main.ff6af1f8.chunk.css"
+  },
+  {
+    "revision": "fd60d4255943143f07c8",
+    "url": "/static/js/2.e9efd565.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e9efd565.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf37ad34fb09310e758e",
+    "url": "/static/js/main.7cb26852.chunk.js"
+  },
+  {
+    "revision": "2e60dea91f8c043f688d",
+    "url": "/static/js/runtime-main.985cb9c8.js"
+  }
+]);
